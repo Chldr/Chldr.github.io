ff99c650aa5578b79d3ca5d65065240414d450d5
Qiu Xing <children24563736@163.com>
2018-12-03 01:22:29 +0800

    init

---
+++ precache-manifest.6d4f8c0cc0ba0038773116ffafcc1466.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0dc9854367df18cf07e",
+    "url": "/static/css/main.557d98b1.chunk.css"
+  },
+  {
+    "revision": "a0dc9854367df18cf07e",
+    "url": "/static/js/main.a0dc9854.chunk.js"
+  },
+  {
+    "revision": "85b583238376b078f028",
+    "url": "/static/js/1.85b58323.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bd4bcbe382430f84a49fd3679b04af1b",
+    "url": "/static/media/calendar_icon.bd4bcbe3.png"
+  },
+  {
+    "revision": "98dbd05b41ab9772a898a4bec5f3d92e",
+    "url": "/index.html"
+  }
+];
