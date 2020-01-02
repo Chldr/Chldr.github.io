2015a6d544a8a8dcb056037a92eb0fae903fe90e
qiuxing <qiuxing@icloudfuture.com>
2020-01-02 11:22:36 +0800

    add fund-manage

---
+++ fund-manage/precache-manifest.4cfe790c397c93e535057348532cb58a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2434e5a91fb21e0da30bb1adbb488de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afbd44005a3bbc869164",
+    "url": "/static/css/2.e4c038d9.chunk.css"
+  },
+  {
+    "revision": "8440f3a733ea3ac96103",
+    "url": "/static/css/main.092a689d.chunk.css"
+  },
+  {
+    "revision": "afbd44005a3bbc869164",
+    "url": "/static/js/2.e5d7f323.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.e5d7f323.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8440f3a733ea3ac96103",
+    "url": "/static/js/main.68b8b85e.chunk.js"
+  },
+  {
+    "revision": "33b15c216c1b415b492b",
+    "url": "/static/js/runtime-main.ec516716.js"
+  }
+]);
